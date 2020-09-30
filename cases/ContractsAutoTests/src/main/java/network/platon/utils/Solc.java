f9bececb0f22bf29dddc307adedd92eb61e8053f
wuyang <wuyang@juzix.net>
2020-09-30 11:37:33 +0800

    update

--- cases/ContractsAutoTests/src/main/java/network/platon/utils/Solc.java
+++ cases/ContractsAutoTests/src/main/java/network/platon/utils/Solc.java
@@ -4,6 +4,8 @@
 //
 
 package network.platon.utils;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
@@ -26,6 +28,7 @@ import java.util.Iterator;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+@Slf4j
 public class Solc {
 
     public static ArrayList<String> solcList = new ArrayList(Arrays.asList("0.4.12", "0.4.13", "0.4.14", "0.4.15", "0.4.16", "0.4.17", "0.4.18", "0.4.19", "0.4.20", "0.4.21", "0.4.22", "0.4.23", "0.4.24", "0.4.25", "0.4.26", "0.5.0", "0.5.1", "0.5.2", "0.5.3", "0.5.4", "0.5.5", "0.5.6", "0.5.7", "0.5.8", "0.5.9", "0.5.10", "0.5.11", "0.5.12", "0.5.13", "0.5.14", "0.5.15", "0.5.17", "0.6.0", "0.6.12", "0.7.1"));
@@ -35,6 +38,7 @@ public class Solc {
 
     public static String pullSolcBin(String contractPath) throws Exception {
         String versionContent = readFile(contractPath);
+        log.info(versionContent);
         String version = parseSolcVersion(versionContent);
         System.out.println("Found solc compiler version: " + version);
         if (version.equals("")) {
